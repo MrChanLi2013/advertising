8986149baa73e64b1eec048806d0a6c28984ec1e
Chan.li <mr.chenli2013@gmail.com>
2016-12-26 17:37:14 +0800

    导航条设置

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,10 @@
 $(function () {
-    $('.dropdown').hover(function(){
+    $('.dropdown').hover(function () {
         $('.dropdown-toggle', this).trigger('click');
     });
+
+    var links = window.location.pathname.split('/');
+    var secondaryUrl = links[2];
+    $('#js-menu').find('li.dropdown').removeClass('active');
+    $('#js-' + secondaryUrl).addClass('active');
 });
