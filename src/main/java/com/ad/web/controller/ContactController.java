f28ddb60564e920f039d24cb058dcccf70324890
Chan.li <mr.chenli2013@gmail.com>
2016-12-27 10:59:11 +0800

    联系我们

--- src/main/java/com/ad/web/controller/ContactController.java
+++ src/main/java/com/ad/web/controller/ContactController.java
@@ -1,9 +1,11 @@
 package com.ad.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RequestMapping("/ad")
+@Controller
 public class ContactController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/contact")
