caa79b564e6b03d4f14bd24331c39b3556a89cb5
Chan.li <mr.chenli2013@gmail.com>
2017-02-14 11:22:43 +0800

    update

--- src/main/java/com/ad/dao/NewsDao.java
+++ src/main/java/com/ad/dao/NewsDao.java
@@ -5,6 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface NewsDao extends CrudRepository<News, Integer> {
     Page<News> findAll(Pageable pageable);
+
+    News findFirstByOrderByCreatedAtDesc();
+
+    List<News> findAllByOrderByCreatedAtDesc();
 }
--- src/main/java/com/ad/web/controller/NewController.java
+++ src/main/java/com/ad/web/controller/NewController.java
@@ -1,14 +1,37 @@
 package com.ad.web.controller;
 
+import com.ad.dao.NewsDao;
+import com.ad.entity.News;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @RequestMapping(value = "/ad")
 @Controller
 public class NewController {
+    @Autowired
+    private NewsDao newsDao;
+
+
     @RequestMapping(value = "/news", method = RequestMethod.GET)
-    public String index() {
+    public String index(Model model, @RequestParam(value = "id", defaultValue = "0", required = false) Integer id) {
+        News news;
+        if (id == null || id == 0) {
+            news = newsDao.findFirstByOrderByCreatedAtDesc();
+        } else {
+            news = newsDao.findOne(id);
+        }
+        List<News> newsList = newsDao.findAllByOrderByCreatedAtDesc();
+        if (news == null) {
+            news = new News("", "");
+        }
+        model.addAttribute("news", news);
+        model.addAttribute("newsList", newsList);
         return "news";
     }
 }
